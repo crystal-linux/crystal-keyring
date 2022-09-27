7afa8a5871448b28ce0de2ba64b177d13e711f55
Michal S <michal@tar.black>
2022-09-27 11:32:54 +0100

    Add new key for Michal S.

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@ echo > crystal.gpg
 
 pushd pubkeys
 
-for key in $(ls)
+for key in $(ls -1 | sed -z '$ s/\n$//')
 do
 	echo $key
+	echo --- $key --- >> ../crystal.gpg
+	echo >> ../crystal.gpg
 	cat $key >> ../crystal.gpg
+	echo >> ../crystal.gpg
+	echo >> ../crystal.gpg
 done
 
 popd
